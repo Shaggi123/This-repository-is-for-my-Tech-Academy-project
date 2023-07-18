340ec1ffd81f39874758d2714daf9ca47ddccfd3
Rosaline <130102218+Shaggi123@users.noreply.github.com>
2023-07-18 10:40:11 -0700

    Operator assignment was submitted.

---
+++ OperatorsAssignmentSubmission/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace OperatorsAssignmentSubmission
+{
+    public class Employee
+    {
+    }
+}
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Overload the "==" operator to compare Employee objects by their Id property
+    public static bool operator ==(Employee emp1, Employee emp2)
+    {
+        if (ReferenceEquals(emp1, emp2))
+            return true;
+
+        if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null))
+            return false;
+
+        return emp1.Id == emp2.Id;
+    }
+
+    // Overload the "!=" operator to complement the "==" operator
+    public static bool operator !=(Employee emp1, Employee emp2)
+    {
+        return !(emp1 == emp2);
+    }
+}
